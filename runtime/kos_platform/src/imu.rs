a0b1b1f0c55bbec2612145256c79f208088d4c37
Wesley Maa <wesley.maa@gmail.com>
2025-01-07 16:06:12 -0800

    fmt

--- runtime/kos_platform/src/imu.rs
+++ runtime/kos_platform/src/imu.rs
@@ -1,5 +1,6 @@
 use async_trait::async_trait;
 
+use crate::firmware::QMI8658;
 use eyre::Result;
 use kos_core::{
     google_proto::longrunning::Operation,
@@ -10,7 +11,6 @@ use linux_bno055::{Bno055, OperationMode};
 use std::{sync::Arc, time::Duration};
 use tokio::sync::Mutex;
 use tracing::{debug, error, info};
-use crate::firmware::QMI8658;
 
 pub struct ZBotIMU {
     imu: Arc<Mutex<Bno055>>,
@@ -19,9 +19,9 @@ pub struct ZBotIMU {
 impl ZBotIMU {
     pub fn new(i2c_bus: &str) -> Result<Self> {
         info!("Initializing ZerothIMU with I2C bus: {}", i2c_bus);
-        
+
         let imu = Bno055::new(i2c_bus)?;
-        
+
         Ok(Self {
             imu: Arc::new(Mutex::new(imu)),
         })
@@ -42,7 +42,7 @@ impl IMU for ZBotIMU {
         let accel = imu.get_accelerometer()?;
         let gyro = imu.get_gyroscope()?;
         let mag = imu.get_magnetometer().ok();
-        
+
         Ok(ImuValuesResponse {
             accel_x: accel.x as f64,
             accel_y: accel.y as f64,
@@ -60,7 +60,7 @@ impl IMU for ZBotIMU {
     async fn get_euler(&self) -> Result<EulerAnglesResponse> {
         let mut imu = self.imu.lock().await;
         let euler = imu.get_euler_angles()?;
-        
+
         Ok(EulerAnglesResponse {
             roll: euler.roll as f64,
             pitch: euler.pitch as f64,
@@ -72,7 +72,7 @@ impl IMU for ZBotIMU {
     async fn get_quaternion(&self) -> Result<QuaternionResponse> {
         let mut imu = self.imu.lock().await;
         let quat = imu.get_quaternion()?;
-        
+
         Ok(QuaternionResponse {
             w: quat.w as f64,
             x: quat.x as f64,
@@ -84,14 +84,14 @@ impl IMU for ZBotIMU {
 
     async fn calibrate(&self) -> Result<Operation> {
         info!("Starting IMU calibration");
-        
+
         let mut imu = self.imu.lock().await;
 
         imu.reset()?;
         let cal_status = imu.get_calibration_status()?;
-        
+
         debug!(
-            "Calibration Status - Sys: {}, Gyro: {}, Accel: {}, Mag: {}", 
+            "Calibration Status - Sys: {}, Gyro: {}, Accel: {}, Mag: {}",
             (cal_status >> 6) & 0x03,
             (cal_status >> 4) & 0x03,
             (cal_status >> 2) & 0x03,
@@ -115,7 +115,7 @@ impl IMU for ZBotIMU {
         max_accel: Option<f32>,
     ) -> Result<ActionResponse> {
         let mut imu = self.imu.lock().await;
-        
+
         match imu.reset() {
             Ok(_) => {
                 // Reset successful, now set mode back to NDOF
@@ -129,7 +129,7 @@ impl IMU for ZBotIMU {
                         }),
                     });
                 }
-                
+
                 Ok(ActionResponse {
                     success: true,
                     error: None,
