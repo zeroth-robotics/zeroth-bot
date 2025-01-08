e25edfc4a95fd981aeac9d3f6a39a11ed8bed827
Denys Bezmenov <denys@kscale.dev>
2025-01-08 02:02:32 +0000

    dont crash on error imu

--- runtime/kos_platform/src/imu.rs
+++ runtime/kos_platform/src/imu.rs
@@ -11,18 +11,42 @@ use tokio::sync::Mutex;
 use tracing::{debug, error, info};
 
 pub struct ZBotIMU {
-    imu: Arc<Mutex<Bno055>>,
+    imu: Arc<Mutex<Option<Bno055>>>,
 }
 
 impl ZBotIMU {
-    pub fn new(i2c_bus: &str) -> Result<Self> {
+    pub fn new(i2c_bus: &str) -> Self {
         info!("Initializing ZerothIMU with I2C bus: {}", i2c_bus);
 
-        let imu = Bno055::new(i2c_bus)?;
+        let imu = match Bno055::new(i2c_bus) {
+            Ok(imu) => Some(imu),
+            Err(e) => {
+                error!("Failed to initialize IMU: {}", e);
+                None
+            }
+        };
 
-        Ok(Self {
+        Self {
             imu: Arc::new(Mutex::new(imu)),
-        })
+        }
+    }
+
+    async fn get_imu_error() -> ImuValuesResponse {
+        ImuValuesResponse {
+            accel_x: 0.0,
+            accel_y: 0.0,
+            accel_z: 0.0,
+            gyro_x: 0.0,
+            gyro_y: 0.0,
+            gyro_z: 0.0,
+            mag_x: None,
+            mag_y: None,
+            mag_z: None,
+            error: Some(Error {
+                code: ErrorCode::HardwareFailure as i32,
+                message: "IMU not initialized".to_string(),
+            }),
+        }
     }
 }
 
@@ -35,36 +59,65 @@ impl Default for ZBotIMU {
 #[async_trait]
 impl IMU for ZBotIMU {
     async fn get_values(&self) -> Result<ImuValuesResponse> {
-        let mut imu = self.imu.lock().await;
+        let mut imu_lock = self.imu.lock().await;
+        
+        let Some(ref mut imu) = *imu_lock else {
+            return Ok(Self::get_imu_error().await);
+        };
 
-        let accel = imu.get_accelerometer()?;
-        let gyro = imu.get_gyroscope()?;
-        let mag = imu.get_magnetometer().ok();
-
-        Ok(ImuValuesResponse {
-            accel_x: accel.x as f64,
-            accel_y: accel.y as f64,
-            accel_z: accel.z as f64,
-            gyro_x: gyro.x as f64,
-            gyro_y: gyro.y as f64,
-            gyro_z: gyro.z as f64,
-            mag_x: mag.as_ref().map(|m| m.x as f64),
-            mag_y: mag.as_ref().map(|m| m.y as f64),
-            mag_z: mag.as_ref().map(|m| m.z as f64),
-            error: None,
-        })
+        match (imu.get_accelerometer(), imu.get_gyroscope()) {
+            (Ok(accel), Ok(gyro)) => {
+                let mag = imu.get_magnetometer().ok();
+                
+                Ok(ImuValuesResponse {
+                    accel_x: accel.x as f64,
+                    accel_y: accel.y as f64,
+                    accel_z: accel.z as f64,
+                    gyro_x: gyro.x as f64,
+                    gyro_y: gyro.y as f64,
+                    gyro_z: gyro.z as f64,
+                    mag_x: mag.as_ref().map(|m| m.x as f64),
+                    mag_y: mag.as_ref().map(|m| m.y as f64),
+                    mag_z: mag.as_ref().map(|m| m.z as f64),
+                    error: None,
+                })
+            }
+            _ => Ok(Self::get_imu_error().await)
+        }
     }
 
     async fn get_euler(&self) -> Result<EulerAnglesResponse> {
-        let mut imu = self.imu.lock().await;
-        let euler = imu.get_euler_angles()?;
+        let mut imu_lock = self.imu.lock().await;
+        
+        let Some(ref mut imu) = *imu_lock else {
+            return Ok(EulerAnglesResponse {
+                roll: 0.0,
+                pitch: 0.0,
+                yaw: 0.0,
+                error: Some(Error {
+                    code: ErrorCode::HardwareFailure as i32,
+                    message: "IMU not initialized".to_string(),
+                }),
+            });
+        };
 
-        Ok(EulerAnglesResponse {
-            roll: euler.roll as f64,
-            pitch: euler.pitch as f64,
-            yaw: euler.yaw as f64,
-            error: None,
-        })
+        match imu.get_euler_angles() {
+            Ok(euler) => Ok(EulerAnglesResponse {
+                roll: euler.roll as f64,
+                pitch: euler.pitch as f64,
+                yaw: euler.yaw as f64,
+                error: None,
+            }),
+            Err(e) => Ok(EulerAnglesResponse {
+                roll: 0.0,
+                pitch: 0.0,
+                yaw: 0.0,
+                error: Some(Error {
+                    code: ErrorCode::HardwareFailure as i32,
+                    message: e.to_string(),
+                }),
+            })
+        }
     }
 
     async fn get_quaternion(&self) -> Result<QuaternionResponse> {
