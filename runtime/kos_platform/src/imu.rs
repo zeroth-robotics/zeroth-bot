7acb81423749c75cdb6d6b30e828c4ba6ddda962
Wesley Maa <wesley.maa@gmail.com>
2025-01-09 10:25:49 -0800

    update

--- runtime/kos_platform/src/imu.rs
+++ runtime/kos_platform/src/imu.rs
@@ -1,10 +1,9 @@
 use async_trait::async_trait;
 
-use crate::firmware::QMI8658;
 use eyre::Result;
 use kos_core::{
     google_proto::longrunning::Operation,
-    hal::{EulerAnglesResponse, ImuValuesResponse, QuaternionResponse, IMU},
+    hal::{EulerAnglesResponse, ImuAdvancedValuesResponse, ImuValuesResponse, QuaternionResponse, IMU},
     kos_proto::common::{ActionResponse, Error, ErrorCode},
 };
 use linux_bno055::{Bno055, OperationMode};
@@ -42,7 +41,6 @@ impl IMU for ZBotIMU {
         let accel = imu.get_accelerometer()?;
         let gyro = imu.get_gyroscope()?;
         let mag = imu.get_magnetometer().ok();
-        let grav = imu.get_gravity_vector().ok();
 
         Ok(ImuValuesResponse {
             accel_x: accel.x as f64,
@@ -54,9 +52,24 @@ impl IMU for ZBotIMU {
             mag_x: mag.as_ref().map(|m| m.x as f64),
             mag_y: mag.as_ref().map(|m| m.y as f64),
             mag_z: mag.as_ref().map(|m| m.z as f64),
+            error: None,
+        })
+    }
+
+    async fn get_advanced_values(&self) -> Result<ImuAdvancedValuesResponse> {
+        let mut imu = self.imu.lock().await;
+
+        let lin_acc = imu.get_linear_acceleration().ok();
+        let grav = imu.get_gravity_vector().ok();
+
+        Ok(ImuAdvancedValuesResponse {
+            lin_acc_x: lin_acc.as_ref().map(|g| g.x as f64),
+            lin_acc_y: lin_acc.as_ref().map(|g| g.y as f64),
+            lin_acc_z: lin_acc.as_ref().map(|g| g.z as f64),
             grav_x: grav.as_ref().map(|g| g.x as f64),
             grav_y: grav.as_ref().map(|g| g.y as f64),
             grav_z: grav.as_ref().map(|g| g.z as f64),
+            temp: imu.get_temperature().ok().map(|t| t as f64),
             error: None,
         })
     }
